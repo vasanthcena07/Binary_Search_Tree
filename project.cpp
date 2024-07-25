b7613f79f19c8ff8d1c55dcf87acf2c65f2318bc
N.Vasanth kumar <119026322+vasanthcena07@users.noreply.github.com>
2024-07-25 15:25:42 +0530

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -1,1 +1,130 @@
+#include <iostream>
 
+using namespace std;
+
+struct TreeNode {
+    int data;
+    TreeNode* left;
+    TreeNode* right;
+};
+
+TreeNode* CreateNode(int data) {
+    TreeNode* newNode = new TreeNode;
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+TreeNode* insert(TreeNode* root, int value) {
+    if (root == NULL) {
+        return CreateNode(value);
+    }
+
+    if (value < root->data) {
+        root->left = insert(root->left, value);
+    } else if (value > root->data) {
+        root->right = insert(root->right, value);
+    }
+
+    return root;
+}
+
+TreeNode* minValueNode(TreeNode* node) {
+    TreeNode* current = node;
+    while (current && current->left != NULL) {
+        current = current->left;
+    }
+    return current;
+}
+
+TreeNode* deleteNode(TreeNode* root, int value) {
+    if (root == NULL) {
+        return root;
+    }
+
+    if (value < root->data) {
+        root->left = deleteNode(root->left, value);
+    } else if (value > root->data) {
+        root->right = deleteNode(root->right, value);
+    } else {
+        if (root->left == NULL) {
+            TreeNode* temp = root->right;
+            delete root;
+            return temp;
+        } else if (root->right == NULL) {
+            TreeNode* temp = root->left;
+            delete root;
+            return temp;
+        }
+
+        TreeNode* temp = minValueNode(root->right);
+        root->data = temp->data;
+        root->right = deleteNode(root->right, temp->data);
+    }
+
+    return root;
+}
+
+TreeNode* search(TreeNode* root, int value) {
+    if (root == NULL || root->data == value) {
+        return root;
+    }
+
+    if (value < root->data) {
+        return search(root->left, value);
+    }
+
+    return search(root->right, value);
+}
+
+void inorder(TreeNode* root) {
+    if (root != NULL) {
+        inorder(root->left);
+        cout << root->data << " ";
+        inorder(root->right);
+    }
+}
+
+int main() {
+    TreeNode* root = NULL;
+    int choice, value;
+
+    do {
+        cout << "\n1. Insert\n2. Delete\n3. Search\n4. Inorder Traversal\n5. Exit\nEnter your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+        case 1:
+            cout << "Enter value to insert: ";
+            cin >> value;
+            root = insert(root, value);
+            break;
+        case 2:
+            cout << "Enter value to delete: ";
+            cin >> value;
+            root = deleteNode(root, value);
+            break;
+        case 3:
+            cout << "Enter value to search: ";
+            cin >> value;
+            if (search(root, value) != NULL)
+                cout << value << " is present in the tree\n";
+            else
+                cout << value << " is not present in the tree\n";
+            break;
+        case 4:
+            cout << "Inorder traversal: ";
+            inorder(root);
+            cout << endl;
+            break;
+        case 5:
+            cout << "Exiting...\n";
+            break;
+        default:
+            cout << "Invalid choice! Please enter a valid option.\n";
+        }
+    } while (choice != 5);
+
+    return 0;
+}
